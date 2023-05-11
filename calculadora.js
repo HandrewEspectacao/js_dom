f88f72ee9f42628f6b8af7f7fad04155e478c4ee
HandrewEspectacao <handrewes.99@gmail.com>
2023-05-11 19:21:11 -0300

    fiz calculadora com js feia kkkk

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,15 @@
-//estoy coringando tentando pensar na logica desse atividade
+valor1 = document.getElementById("v1");
+valor2 = document.getElementById("v2");
+resultado = document.getElementById("res");
+function soma(){
+    resultado.innerHTML = `O resultado ${Number(valor1.value) + Number(valor2.value)} `
+}
+function sub(){
+    resultado.innerHTML = `O resultado ${Number(valor1.value) - Number(valor2.value)} `
+}
+function mult(){
+    resultado.innerHTML = `O resultado ${Number(valor1.value) * Number(valor2.value)} `
+}
+function div(){
+    resultado.innerHTML = `O resultado ${Number(valor1.value) / Number(valor2.value)} `
+}
