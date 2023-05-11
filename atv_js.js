25b03a7aecb9126ebb23931acf9871b9200fbb8b
HandrewEspectacao <handrewes.99@gmail.com>
2023-05-11 20:49:50 -0300

    calculadora

--- atv_js.js
+++ atv_js.js
@@ -3,10 +3,12 @@ let ligar = document.getElementById("ligar")
 //botao esta mudando a imagens
 ligar.addEventListener("click", ()=>{
     document.getElementById("lampada").src = "imagens/pic_bulbon.gif"
+    document.body.style.background = "initial"
 })
 //fazendo uma variavel para o botão desligar
 let desligar = document.getElementById("off")
 //botão para "desligar a lampada"
 desligar.addEventListener("click", ()=>{
     document.getElementById("lampada").src = "imagens/pic_bulboff.gif"
+    document.body.style.background = "black"
 })
